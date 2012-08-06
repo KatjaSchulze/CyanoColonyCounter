defd25b76e56361fe8f4a003af0520040623c99e
KatjaSchulze <kschulze@th-wildau.de>
2012-08-06 13:56:23 +0200

    commit

--- src/CountCyanoPlate_.java
+++ src/CountCyanoPlate_.java
@@ -1,3 +1,23 @@
+/**
+CountCyanoPlate - Counts the colonies of plated cyanobacteria in microscopic images based on the phycocyanin fluorescence.
+
+Copyright (C) 2012  Katja Schulze (kschulze@th-wildau.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
